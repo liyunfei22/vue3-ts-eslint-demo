ea4ac82c9dcbc88a2cbe497d67da6cf26c3361ee
李云飞 <liyunfei02@daojia-inc.com>
2022-03-18 23:34:03 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 
 const locale = zhCn;
 </script>
-
 <template>
   <div class="m-5">
+    <div>ss</div>
+    <div>ssssss sssss</div>
     <el-config-provider :locale="locale">
       <router-view></router-view>
     </el-config-provider>
